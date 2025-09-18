701059a38b99222bd97fac5bc516301edf4e3387
Sina Gharali <sinagharali.next@gmail.com>
2025-09-18 16:37:07 +0330

    Migration for Adding membership table

--- alembic/env.py
+++ alembic/env.py
@@ -14,6 +14,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "s
 from session.model import Session
 from user.model import User
 from board.model import Board
+from membership.model import Membership
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

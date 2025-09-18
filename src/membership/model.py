7ca35dff864b4a75b49101add4ebb2ef5006691a
Sina Gharali <sinagharali.next@gmail.com>
2025-09-18 16:12:17 +0330

    Add membership model

---
+++ src/membership/model.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from enum import Enum
+from typing import TYPE_CHECKING, Optional
+from uuid import UUID
+
+from sqlalchemy import Column, DateTime, ForeignKey
+from sqlmodel import Field, Relationship
+
+from common.models.base_orm_model import BaseORM
+
+if TYPE_CHECKING:
+    from board.model import Board
+    from user.model import User
+
+
+class MembershipRole(str, Enum):
+    ADMIN = "admin"
+    MEMBER = "member"
+
+
+class Membership(BaseORM, table=True):
+    """
+    Mmebership Model.
+
+    Fields:
+    - **id_** : Primary key (UUID)
+    """
+
+    __tablename__: str = "memberships"
+    member_id: UUID = Field(
+        default=None,
+        sa_column=Column(ForeignKey("users.id_"), primary_key=True),
+    )
+    member: Optional["User"] = Relationship()
+    board_id: UUID = Field(
+        default=None,
+        sa_column=Column(ForeignKey("boards.id_"), primary_key=True),
+    )
+    board: Optional["Board"] = Relationship()
+    role: MembershipRole
+    created_at: datetime = Field(sa_column=Column(DateTime(timezone=True)))
+    updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True)))

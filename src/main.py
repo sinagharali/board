a6ba9deaa7205ac2a04d1652e1b6ce8a479addac
Sina Gharali <sinagharali.next@gmail.com>
2025-09-24 16:01:14 +0330

    Add invitation route

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from acl.client import OpenFgaClientSingleton
 from auth.api.v1.router import router as auth_router
 from board.api.v1.router import router as board_router
 from core.error_handler import register_exception_handlers
+from invitation.api.v1.router import router as invitation_router
 from logger import logger
 
 
@@ -20,6 +21,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(board_router)
+app.include_router(invitation_router)
 
 register_exception_handlers(app)
 logger.info("Starting API...")

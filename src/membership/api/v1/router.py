369bd0c7614e5edfd09bcf995443411c4745cc10
Sina Gharali <sinagharali.next@gmail.com>
2025-09-24 16:41:56 +0330

    add membership endpoints

--- src/membership/api/v1/router.py
+++ src/membership/api/v1/router.py
@@ -1,9 +1,73 @@
-from fastapi import APIRouter
+from typing import Annotated
+from uuid import UUID
+
+from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 
-router = APIRouter()
+from auth.provider import get_current_user
+from user.model import User
+
+router = APIRouter(tags=["Mmebership"])
 
 
 @cbv(router)
 class MembershipCBV:
-    pass
+    @router.get("/memberships/")
+    async def get_memberships(
+        self,
+        board_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+        limit: int = 50,
+        offset: int = 0,
+        role: str | None = None,  # optional filter by role
+    ): ...
+
+    @router.get("/memberships/{member_id}/")
+    async def get_membership(
+        self,
+        board_id: UUID,
+        member_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+    ): ...
+
+    @router.get("/boards/{board_id}/memberships/{member_id}/")
+    async def get_board_membership(
+        self,
+        board_id: UUID,
+        member_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+    ): ...
+
+    @router.patch("/boards/{board_id}/memberships/{member_id}/upgrade-to-admin/")
+    async def upgrade_to_admin(
+        self,
+        board_id: UUID,
+        member_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+        roles: list[str] | None = None,  # optional roles to assign on upgrade
+    ): ...
+
+    @router.patch("/boards/{board_id}/memberships/{member_id}/add-roles/")
+    async def add_roles_to_admin(
+        self,
+        board_id: UUID,
+        member_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+        roles: list[str],
+    ): ...
+
+    @router.patch("/boards/{board_id}/memberships/{member_id}/downgrade-to-member/")
+    async def downgrade_to_member(
+        self,
+        board_id: UUID,
+        member_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+    ): ...
+
+    @router.delete("/boards/{board_id}/memberships/{member_id}/")
+    async def revoke_membership(
+        self,
+        board_id: UUID,
+        member_id: UUID,
+        user: Annotated[User, Depends(get_current_user)],
+    ): ...

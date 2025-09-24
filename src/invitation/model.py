c0545d53f282f4aae44bbfd7daed75490a67e454
Sina Gharali <sinagharali.next@gmail.com>
2025-09-24 15:59:30 +0330

    Update invitation model , remove status and membership role

--- src/invitation/model.py
+++ src/invitation/model.py
@@ -1,16 +1,11 @@
 from datetime import datetime
 from uuid import UUID
 
-from sqlalchemy import Column, DateTime, Enum, ForeignKey
+from sqlalchemy import Column, DateTime, ForeignKey
 from sqlmodel import Field
 
 from common.models.base_orm_model import BaseORM
 from membership.model import MembershipRole
-
-
-class InvitationStatus(str, Enum):  # noqa: SLOT000
-    PENDING = "pending"
-    ACCEPTED = "accepted"
 
 
 class Invitation(BaseORM, table=True):
@@ -21,8 +16,6 @@ class Invitation(BaseORM, table=True):
     - **id_** = Primary Key (UUID)
     - **invited_by** : Foriegn key to User (UUID)
     - **invitee_email** : email belongs to the invited person (str)
-    - **membership_role** : The role of the user (MembershipRole)
-    - **status** : Status of the invitation: pending or accepted (InvitationStatus)
     - **target_id** : Foriegn key to board (UUID)
     - **created_at** : When Membership Model is Created (DateTime)
 
@@ -32,7 +25,5 @@ class Invitation(BaseORM, table=True):
     id_: UUID = Field(primary_key=True)
     invited_by: UUID = Field(sa_column=Column(ForeignKey("users.id_")))
     invitee_email: str
-    membership_role: MembershipRole
-    status: InvitationStatus
     target_id: UUID = Field(sa_column=Column(ForeignKey("boards.id_")))
     created_at: datetime = Field(sa_column=Column(DateTime(timezone=True)))
